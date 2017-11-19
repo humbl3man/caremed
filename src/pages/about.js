b3f96faea7774c0982f1ac53cf55e719ac4eb27c
humbl3man <konstantindev925@gmail.com>
2017-11-19 12:12:47 -0800

    make changes to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,9 @@ import aboutPic from './../images/about.jpg';
 export default () => (
 	<Container>
 		<div className="site-content">
-			<h1>About us</h1>
+			<h1>
+				<strong>About Us</strong>
+			</h1>
 
 			<div className="about">
 				<div className="about__image">
@@ -16,19 +18,19 @@ export default () => (
 					<p>
 						Care MED Transport is a privately owned and operated
 						company providing Non-emergency transportation to the
-						community of Orange County and LA County Care Med
-						Transport was founded on 2016 based on experience that a
-						lot of people needed a service for transportation to go
-						for doctor's appointment, medical treatments and other
-						medical necessity.
+						community of Greater Los Angeles and Orange County. Care
+						MED Transport was founded in 2016 based on experience
+						that many people needed a service for transportation to
+						go for doctor's appointment, medical treatments and
+						other medical necessity.
 					</p>
 					<p>
 						We decided to establish our own transportation services
 						to be of service to the community. Care MED Transport is
 						operated by well trained and experienced staffs over 10
 						years in medical field. Our Vision and Dedication of our
-						team to build a greatest medical transportation to the
-						community and role model for american health care.
+						team are to build the greatest medical transportation to
+						the community and role model for American Health Care.
 					</p>
 				</div>
 			</div>
